2c4ca77a6f4b4282a3d640eb08b0c31140a0edd7
Divakar Gaur <85343000+divakargaur@users.noreply.github.com>
2023-09-25 16:56:45 +0000

    codeExecution

---
+++ 03_basics/how_code_executes.js
@@ -0,0 +1,16 @@
+/*
+JAVASCRIPT EXECUTION CONTEXT
+----------------------------
+
+1. Global Execution Context
+2. Function Execution Context
+3. Eval Execution Context
+*/
+
+/*
+PHASES
+------
+
+1. Memory Creation Phase
+2. Execution Phase
+*/
