eba6e893796a195c05d70d50da8e2586e88937b6
Divakar Gaur <85343000+divakargaur@users.noreply.github.com>
2023-09-30 08:11:36 +0000

    working on proj2

--- 07_projects/02_BMIcalculator/script.js
+++ 07_projects/02_BMIcalculator/script.js
@@ -1,1 +1,13 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form")
+
+form.addEventListener("submit", element => {
+    element.preventDefault()
+
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const output = parseInt(document.querySelector("#output").value)
+
+    if (height === "" || height <= 0 || isNaN(height)) {
+        output.innerHTML = "ENTER A VALID HEIGHT"
+    }
+})
