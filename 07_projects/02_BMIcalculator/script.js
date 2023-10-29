995853217b039305f102e38d2f3907fdb3591c62
Divakar Gaur <divakargaur270@gmail.com>
2023-10-29 13:16:17 +0530

    proj2 done

--- 07_projects/02_BMIcalculator/script.js
+++ 07_projects/02_BMIcalculator/script.js
@@ -1,13 +1,16 @@
 const form = document.querySelector("form")
 
-form.addEventListener("submit", element => {
-    element.preventDefault()
+form.addEventListener("submit", event => {
+    event.preventDefault()
 
-    const height = parseInt(document.querySelector("#height").value)
-    const weight = parseInt(document.querySelector("#weight").value)
-    const output = parseInt(document.querySelector("#output").value)
+    let height = parseFloat(document.querySelector("#height").value);
+    let weight = parseFloat(document.querySelector("#weight").value);
+    let result = document.querySelector("#result");
 
-    if (height === "" || height <= 0 || isNaN(height)) {
-        output.innerHTML = "ENTER A VALID HEIGHT"
+    if (height === "" || height <= 0 || isNaN(height) || weight === "" || weight <= 0 || isNaN(weight)) {
+        result.innerHTML = "ENTER A VALID HEIGHT & WEIGHT";
+    } else {
+        let BMI = (weight / (height * height));
+        result.innerHTML = BMI
     }
-})
+});
