1caf36912e8d98fd0e0d6aa6499001813f0b5939
Divakar Gaur <divakargaur270@gmail.com>
2023-10-31 17:54:19 +0530

    event & async basics

---
+++ 08_events/async.js
@@ -0,0 +1,24 @@
+/*
+JavaScript (Default Behavior)
+----------
+-> It is a synchronous language
+-> It is a single threaded language
+
+
+Execution Content
+-----------------
+-> Executes one line of code at a time
+-> Each operation waits for the last one to complete before executing
+
+
+Blocking Code
+-------------
+-> Block the flow of program
+-> Read file synchronous
+
+
+Non Blocking Code
+-----------------
+-> Does not block execution
+-> Read file asynchronous
+*/
