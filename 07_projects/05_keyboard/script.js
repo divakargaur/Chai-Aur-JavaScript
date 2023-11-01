ebb5dea185424f624980ed603cc0af6131e1b67b
Divakar Gaur <divakargaur270@gmail.com>
2023-11-01 11:32:50 +0530

    proj5 complete

--- 07_projects/05_keyboard/script.js
+++ 07_projects/05_keyboard/script.js
@@ -1,1 +1,18 @@
-// 32 min
+const div = document.querySelector("div");
+
+window.addEventListener("keydown", event => {
+    div.innerHTML = `
+    <table>
+        <tr>
+            <th>Key</th>
+            <th>KeyCode</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${event.key === " " ? "Space" : event.key}</td>
+            <td>${event.keyCode}</td>
+            <td>${event.code}</td>
+        </tr>
+    </table>
+    `
+})
